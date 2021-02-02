d061ad311c4948834bd122dbeae70295a38d64e8
Bruno Vercelino da Hora <brunodahora@gmail.com>
2021-02-02 20:44:00 -0300

    Test: Click in the previous month button

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from '../App';
 
 jest.mock('date-fns', () => ({ today: jest.fn() }));
@@ -11,13 +11,15 @@ describe('Calendar App', () => {
         jest.setSystemTime(new Date(2021, 1, 2));
       });
 
-      test('Then I should see the current month and year', () => {
+      beforeEach(() => {
         render(<App />);
+      });
+
+      test('Then I should see the current month and year', () => {
         expect(screen.getByText('February, 2021')).toBeInTheDocument();
       });
 
       test('Then I should see the days of the week', () => {
-        render(<App />);
         expect(screen.getByText('Sunday')).toBeInTheDocument();
         expect(screen.getByText('Monday')).toBeInTheDocument();
         expect(screen.getByText('Tuesday')).toBeInTheDocument();
@@ -28,15 +30,31 @@ describe('Calendar App', () => {
       });
 
       test('Then I should see all days from february and the completing days from previous and next months', () => {
-        render(<App />);
         expect(screen.getByText('31')).toBeInTheDocument();
         expect(screen.getByText('10')).toBeInTheDocument();
-        expect(screen.queryAllByText('1')).toHaveLength(2);
+        expect(screen.queryAllByText('6')).toHaveLength(2);
       });
 
       test('Then I should see the complete calendar', () => {
         const { container } = render(<App />);
         expect(container.firstChild).toMatchSnapshot();
+      });
+
+      describe('When I click in the previous month button', () => {
+        beforeEach(() => {
+          fireEvent.click(screen.getByAltText('Previous Month'));
+        });
+
+        test('Then I should see the current month and year', () => {
+          expect(screen.getByText('January, 2021')).toBeInTheDocument();
+        });
+
+        test('Then I should see all days from january and the completing days from previous and next months', () => {
+          render(<App />);
+          expect(screen.queryAllByText('27')).toHaveLength(2);
+          expect(screen.getByText('10')).toBeInTheDocument();
+          expect(screen.queryAllByText('6')).toHaveLength(2);
+        });
       });
     });
   });
